af9f0ac8e1f39ece7a898903d4afeffcdd37b6d1
Kaycee Lim <sandralim298@gmail.com>
2020-09-15 16:18:32 +0800

    Blog Template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/blog-styles.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -76,5 +77,10 @@
             @yield('content')
         </main>
     </div>
+    <footer class="page-footer font-small stylish-color-light bt-4 mt-4">
+        <div class="footer-copyright py-3 text-center">
+            Copyright 2020: CMS Blogging
+        </div>
+    </footer>
 </body>
 </html>
