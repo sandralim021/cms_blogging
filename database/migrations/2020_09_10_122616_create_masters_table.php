8315d8d1947c14c7e47bddf2e493f12404af600b
Kaycee Lim <sandralim298@gmail.com>
2020-10-05 21:51:46 +0800

    Photo Default Photo Changed

--- database/migrations/2020_09_10_122616_create_masters_table.php
+++ database/migrations/2020_09_10_122616_create_masters_table.php
@@ -18,7 +18,7 @@ class CreateMastersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('role')->default('author');
-            $table->string('photo')->default('user_photo.png');
+            $table->string('photo')->default('user_default.png');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
